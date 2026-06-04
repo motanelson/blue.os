1f1c075e2815a8c6720f756d033bcb2d73002bf6
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-04 18:37:56 +0100

    Add files via upload

--- cd.sh
+++ cd.sh
@@ -3,6 +3,7 @@ mkdir -p CD_root/images
 mkdir -p CD_root/kernel
 cp isolinux.bin CD_root/isolinux/isolinux.bin
 cp isolinux.cfg CD_root/isolinux/isolinux.cfg
+ls CD_root/isolinux > CD_root/isolinux/ls.txt
 cp *.bss CD_root/isolinux/
 cp *.sys CD_root/isolinux/
 cp *.c32 CD_root/isolinux/
@@ -14,6 +15,7 @@ cp *.txt CD_root/isolinux/
 bcc -c -Md libdos.c -o libdos.a
 bcc -x -i -L -Md intro.c -o intro.com
 bcc -x -i -L -Md cat.c -o cat.com
+bcc -x -i -L -Md ls.c -o ls.com
 cp *.c32 CD_root/isolinux/
 cp *.com CD_root/isolinux/
 genisoimage -o myos.iso -input-charset utf-8 -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4  -boot-info-table ./CD_root 
